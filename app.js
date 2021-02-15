f0f74b25423685f158d4eb145050ecd9fbb1b0ea
Mike Gatfield <m.gatfield@whitbywood.com>
2021-02-15 00:32:32 +0000

    modal jazziness

--- app.js
+++ app.js
@@ -12,15 +12,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 router.get("/", (req, res) => {
   var stakeout = whereshouldipaddle();
+  var thenumbers = tellmenumbers();
   res.render("index", {
     title: "Thames Weir Stakeout",
     message: stakeout,
+    numbers: thenumbers,
   });
   console.log("pinged the front page");
-});
-
-router.get("/about", (req, res) => {
-  res.render("about", {});
 });
 
 app.use("/", router);
@@ -28,28 +26,13 @@ app.listen(process.env.port || 3000);
 
 console.log("Alive and kicking at Port 3000");
 
-class Station {
-  constructor(id, url, flow) {
-    this.id = id;
-    this.url = url;
-    this.flow = flow;
-  }
-}
-
 const today = new Date().toJSON().slice(0, 10);
 let yesterday = new Date(Date.now() - 864e5); // 864e5 == 86400000 == 24*60*60*1000
 yesterday = yesterday.toJSON().slice(0, 10);
-var readingqty;
 
-var maids = new Station("2604TH", null, null);
-var taplow = new Station("2603TH", null, null);
-var weirstatus = {
-  Hurley: null,
-  Hambleden: null,
-  Boulters: null,
-  Sunbury: null,
-  Chertsey: null,
-};
+var maids = { id: "2604TH", url: null, dataobj: [], flownow: null };
+var taplow = { id: "2603TH", url: null, dataobj: [], flownow: null };
+var total = { id: null, url: null, dataobj: [], flownow: null };
 
 getflow(maids);
 getflow(taplow);
@@ -60,46 +43,59 @@ function getflow(station) {
     .then((res) => res.json())
     .then(
       (json) => (
-        (station.flow = json.items),
-        console.log("done with data"),
-        console.log(station.flow[0].value),
-        (readingqty = json.items.length)
+        (station.dataobj = json.items),
+        sumflows(),
+        (station.flownow = station.dataobj[0].value)
       )
     );
 }
 
-function whereshouldipaddle() {
-  var wheretogo = [];
-  var totalflow = maids.flow[0].value + taplow.flow[0].value;
+function sumflows() {
+  if (taplow.dataobj[0] != null) {
+    for (
+      var i = 0;
+      i < Math.min(maids.dataobj.length, taplow.dataobj.length);
+      i++
+    ) {
+      total.dataobj.push(maids.dataobj[i]);
+      total.dataobj[i].value = maids.dataobj[i].value + taplow.dataobj[i].value;
+    }
+    total.flownow = total.dataobj[0].value;
+  }
+}
 
-  if (totalflow < 20) {
+function tellmenumbers(){
+ return [
+    "Maids gauge: " + math.round(maids.flownow) + " cumecs (a.k.a. flow thru Boulters)",
+    "Taplow gauge: " + math.round(taplow.flownow) + " cumecs (a.k.a. flow down Jubilee)",
+    "Total: " + math.round(total.flownow) + " cumecs (a.k.a. flow down most weirs inc Hurley)",
+  ];
+}
+
+function whereshouldipaddle() {
+  // Aim of this is to add anything that might be worth checking out to the homepage
+  var wheretogo = [];
+
+  if (total.flownow < 20) {
     wheretogo.push(
       "Summer sun vibes - suncream on and head to Boulters or Chertsey!"
     );
   }
-  if (totalflow < 50 && totalflow > 20) {
+  if (total.flownow < 50 && total.flownow > 20) {
     wheretogo.push("1 gate fun - try out Hambleden for a change?");
   }
-  if (totalflow < 140 && totalflow > 50) {
+  if (total.flownow < 140 && total.flownow > 50) {
     wheretogo.push("Go time - time to get haul ass to Hurley");
   }
-  if (totalflow > 100) {
+  if (total.flownow > 100) {
     wheretogo.push("Sunbury or Shepperton could be worth a butchers");
   }
-  if (totalflow > 140 && totalflow < 170) {
+  if (total.flownow > 140 && total.flownow < 170) {
     wheretogo.push("Might be time to become a Marsh-ian");
   }
-  if (maids.flow[0].value > 150 && maids.flow[0].value < 185) {
+  if (total.flownow > 150 && maids.flownow < 185) {
     wheretogo.push("Time to go big on Boulters!");
   }
-  wheretogo.push(
-    "Maids: " +
-      math.round(maids.flow[0].value) +
-      "cumecs, Taplow: " +
-      math.round(taplow.flow[0].value) +
-      "cumecs, Total: " +
-      math.round(totalflow) +
-      "cumecs"
-  );
+
   return wheretogo;
 }
